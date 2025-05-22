790a69507c1ea19615f95b1daecbc67a19522786
juanporta1 <juanporta.c@gmail.com>
2025-05-22 18:49:30 -0300

    terminando el menu lateral en el responsive, faltan los links

--- apps/frontend/src/components/layout/Header/Header.svelte
+++ apps/frontend/src/components/layout/Header/Header.svelte
@@ -1,19 +1,26 @@
-<script>
+<script lang="ts">
 	import { mainColor } from '../../../store/readables-store';
+	import { Accordion, AccordionItem } from 'svelte-5-ui-lib';
+	import { isOpened } from '../../../store/writeable-store';
+
+	let slideNavBar: HTMLDivElement;
+	const classes = ['-left-full', 'left-0'];
 </script>
 
 <div
-	class="flex h-20 w-full flex-row items-center justify-center"
+	class="hidden h-20 w-full flex-row items-center justify-center sm:inline-flex"
 	style={`background-color: ${$mainColor}`}
 >
-	<div class="flex h-full w-4/5 flex-row items-center justify-center">
-		<div class="flex h-full w-1/2 items-center justify-start gap-4 pl-10">
+	<div class="flex h-full w-10/12 flex-row items-center justify-center">
+		<div
+			class="hidden h-full w-1/2 items-center justify-center gap-4 pl-10 md:inline-flex md:justify-start"
+		>
 			<span class="group relative flex h-8 w-8 cursor-pointer items-center justify-center">
 				<div
 					class="absolute h-full w-full rounded-full bg-red-700 transition group-hover:scale-0"
 				></div>
 				<i
-					class="fa-brands fa-facebook scale-125 transition group-hover:scale-150 group-hover:text-red-700"
+					class="fa-brands fa-facebook scale-125 text-white transition group-hover:scale-150 group-hover:text-red-700"
 				></i>
 			</span>
 			<span class="group relative flex h-8 w-8 cursor-pointer items-center justify-center">
@@ -21,11 +28,11 @@
 					class="absolute h-full w-full rounded-full bg-red-700 transition group-hover:scale-0"
 				></div>
 				<i
-					class="fa-brands fa-instagram scale-125 transition group-hover:scale-150 group-hover:text-red-700"
+					class="fa-brands fa-instagram scale-125 text-white transition group-hover:scale-150 group-hover:text-red-700"
 				></i></span
 			>
 		</div>
-		<div class=" flex h-full w-3/5 items-center justify-end gap-6">
+		<div class="flex h-full w-3/5 items-center justify-center gap-6 md:justify-end">
 			<div
 				class="group flex h-full cursor-pointer items-center justify-center gap-2 transition duration-300"
 			>
@@ -35,11 +42,11 @@
 
 				<div class="flex h-1/2 flex-col items-start justify-center">
 					<span
-						class="h-6 text-lg font-bold text-white transition-colors duration-300 group-hover:text-red-700"
+						class="h-6 font-bold text-white transition-colors duration-300 group-hover:text-red-700 md:text-base lg:text-lg"
 					>
 						Email
 					</span>
-					<span class="text-sm text-white">info@centrodecomercioag.com.ar</span>
+					<span class=" text-white md:text-xs lg:text-sm">info@centrodecomercioag.com.ar</span>
 				</div>
 			</div>
 			<div
@@ -51,19 +58,43 @@
 
 				<div class="flex h-1/2 flex-col items-start justify-center">
 					<span
-						class="h-6 text-lg font-bold text-white transition-colors duration-300 group-hover:text-red-700"
+						class="h-6 font-bold text-white transition-colors duration-300 group-hover:text-red-700 md:text-base lg:text-lg"
 					>
 						Teléfono
 					</span>
-					<span class="text-sm text-white">(03547) 3547446368.</span>
+					<span class="text-white md:text-xs lg:text-sm">(03547)3547446368.</span>
 				</div>
 			</div>
 		</div>
 	</div>
 </div>
 
+<!-- Menu Desplazante -->
+<div
+	bind:this={slideNavBar}
+	class={`absolute ${classes[$isOpened]} z-40 h-screen w-screen bg-white  shadow-2xl shadow-gray-700 transition-all duration-300  sm:hidden`}
+>
+	<div class="flex h-full w-full flex-col">
+		<div class="flex w-full flex-row items-center justify-end p-10">
+			<button
+				aria-label="Close"
+				on:click={(e) => {
+					$isOpened = $isOpened === 0 ? 1 : 0;
+				}}
+			>
+				<i class="fa-solid fa-x text-2xl text-black"></i>
+			</button>
+		</div>
+		<div class="flex flex-col">
+			<div class="px-3.5 py-5">
+				<a href="#a" aria-label="Institucional"> Institucional</a>
+			</div>
+		</div>
+	</div>
+</div>
+
 <nav class="h-28 w-full border-b bg-white shadow-sm">
-	<div class="mx-auto flex h-full max-w-7xl items-center justify-between px-12 py-4">
+	<div class="mx-auto flex h-full w-10/12 items-center justify-between p-4 sm:px-12 sm:py-4">
 		<!-- Logo -->
 		<div class="flex items-center gap-4">
 			<a href="http://centrodecomercioag.com.ar/">
@@ -75,27 +106,41 @@
 			</a>
 		</div>
 
+		<!-- Botón de menú para móviles -->
+		<button
+			aria-label="Menu"
+			on:click={(e) => {
+				$isOpened = $isOpened === 0 ? 1 : 0;
+			}}
+		>
+			<div class="block sm:hidden">
+				<i class="fa-solid fa-bars scale-125 pr-6 text-2xl text-black"></i>
+			</div>
+		</button>
+
 		<!-- Menú -->
-		<ul class="flex h-1/2 items-center space-x-10 text-sm font-medium">
+		<ul class="hidden h-1/2 items-center space-x-10 text-sm font-medium sm:flex">
 			<!-- Institucional Dropdown -->
 			<li class="group relative">
-				<a
-					href="http://centrodecomercioag.com.ar/quienes-somos/"
-					class="h-10 p-4 text-base text-gray-800 transition-colors duration-300 group-hover:text-blue-700"
-				>
-					Institucional
-
-					<i
-						class="fa-solid fa-chevron-up relative top-0.5 scale-90 rotate-180 transition duration-300 group-hover:rotate-0"
+				<div class="flex items-center justify-center gap-2 text-black">
+					<a
+						href="http://centrodecomercioag.com.ar/quienes-somos/"
+						class="py-4 text-base text-gray-800 transition-colors duration-300 group-hover:text-blue-700"
+					>
+						Institucional
+					</a>
+					<!-- <i
+						class="fa-solid fa-chevron-up relative -top-2 -left-1 scale-50 rotate-180 transition duration-300 group-hover:rotate-0"
 						style="top: 1px;"
-					></i>
-				</a>
-				<ul
-					class=" pointer-events-none absolute top-12 z-10 w-40 border border-t-3 border-t-red-700
-                         bg-white opacity-0 shadow-md transition-all duration-300 group-hover:pointer-events-auto group-hover:top-9 group-hover:opacity-100"
+					></i> -->
+				</div>
+
+				<!-- <ul
+					class=" pointer-events-none absolute top-30 z-10 w-40 border border-t-3 border-t-red-700 bg-white
+                         opacity-0 shadow-md transition-all duration-300 group-hover:pointer-events-auto group-hover:top-14 group-hover:opacity-100"
 				>
 					<span
-						class="absolute left-25 -z-10 h-3 w-3 rotate-45 border-t-3 border-l-3 border-t-red-700 border-l-red-700 bg-white"
+						class="absolute left-5 -z-10 h-3 w-3 rotate-45 border-t-3 border-l-3 border-t-red-700 border-l-red-700 bg-white"
 						style="top: -8px;"
 					></span>
 					<li>
@@ -114,27 +159,30 @@
 							Estatuto
 						</a>
 					</li>
-				</ul>
+				</ul> -->
 			</li>
 
-			<hr class="h-full w-0.5 bg-gray-700" />
+			<hr class="h-8 w-0.5 bg-gray-700" />
 			<!-- Hacete socio Dropdown -->
 			<li class="group relative">
-				<a
-					href="http://centrodecomercioag.com.ar/hacete-socio/"
-					class="py-4 text-base text-red-600 transition-all hover:text-red-800"
-					>Hacete socio
-					<i
-						class="fa-solid fa-chevron-up relative -right-0.5 scale-90 rotate-180 transition duration-300 group-hover:rotate-0"
+				<div class="flex items-center justify-center gap-2 text-black">
+					<a
+						href="http://centrodecomercioag.com.ar/hacete-socio/"
+						class="py-4 text-base text-red-600 transition-all hover:text-red-800"
+						>Hacete socio
+					</a>
+					<!-- <i
+						class="fa-solid fa-chevron-up relative -top-4 -left-1 scale-50 rotate-180 transition duration-300 group-hover:rotate-0"
 						style="top: 1px;"
-					></i>
-				</a>
-				<ul
-					class=" pointer-events-none absolute top-12 -left-2 z-10 w-40 border border-t-3 border-t-red-700
-                         bg-white opacity-0 shadow-md transition-all duration-300 group-hover:pointer-events-auto group-hover:top-9 group-hover:opacity-100"
+					></i> -->
+				</div>
+
+				<!-- <ul
+					class=" pointer-events-none absolute top-30 -left-2 z-10 w-40 border border-t-3 border-t-red-700
+                         bg-white opacity-0 shadow-md transition-all duration-300 group-hover:pointer-events-auto group-hover:top-14 group-hover:opacity-100"
 				>
 					<span
-						class="absolute left-25 -z-10 h-3 w-3 rotate-45 border-t-3 border-l-3 border-t-red-700 border-l-red-700 bg-white"
+						class="absolute left-5 -z-10 h-3 w-3 rotate-45 border-t-3 border-l-3 border-t-red-700 border-l-red-700 bg-white"
 						style="top: -8px;"
 					></span>
 					<li>
@@ -155,7 +203,7 @@
 							class="block px-4 py-2 text-black transition hover:bg-gray-100">Capacitaciones</a
 						>
 					</li>
-				</ul>
+				</ul> -->
 			</li>
 
 			<!-- Contacto -->
